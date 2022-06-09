fe1b438cc3d47913807d82e54ea90d5edf93b869
Mateusz Szewczyk <szewczyk.mateusz96@gmail.com>
2022-06-09 12:16:57 +0200

    input field with enter functionality AND number type without those pesky arrows

--- components/input.js
+++ components/input.js
@@ -7,20 +7,24 @@ const Input = () => {
   const enter = (e) => {
     e.preventDefault();
     if (e.keyCode === 13) {
-      console.log("enter");
+      console.log("enter", amountOfCards);
     }
   };
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className='input-section'>
-      <form className='input-form'>
+      <form className='input-form' onSubmit={handleSubmit}>
         <div className='input-container'>
           <label>Wygeneruj Planety Mordo</label>
           <input
-            type='text'
-            placeholder='How many planets do you want to generate?'
+            type='number'
+            placeholder='How many planets do you want to generate? (1-10)'
             value={amountOfCards}
-            // onKeyDown={(e) => enter(e)} 
+            onKeyUp={(e) => {
+              enter(e);
+            }}
             onChange={(e) => {
               setAmountOfCards(e.target.value);
             }}
